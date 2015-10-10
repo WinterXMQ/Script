c7b9fed2cbe624718378d8626c57ffd3fffa1322
WinterXMQ <WinterXMQ@gmail.com>
2015-10-10 16:58:58 +0200

    Fist Done

--- SendMail.py
+++ SendMail.py
@@ -1,14 +1,30 @@
 # coding=utf-8
 __author__ = 'ming'
 from os import system, remove
+import time
 
-def sendErrorInfo(content, email = "WinterXMQ@qq.com", tmpfile = '/tmp/error-html'):
-    content = "Errorpacmsn\n" + content
+def sendErrorInfo(content, email = None, tmpfile = 'error.html'):
+
+    # 错误信息写入文本
+    content = "Testing ..... \n Error\n" + time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())) + "\n" + content
     f = file(tmpfile, "w+")
-    f.write(content)
+    f.write(content.encode("UTF-8"))
     f.close()
-    print('cat %s | mail -s "Antsoul-Error" %s' % (tmpfile, email))
-    system('cat %s | mail -s "Antsoul-Error" %s' % (tmpfile, email))
+    # Email 列表
+    if email is None :
+        email = "WinterXMQ@qq.com", "jonathan.swjtu@gmail.com", "15321689@qq.com"
+    # 发邮件的信息
+    from_name = "Antsoul Test"
+    fr = "root@antsoul.com"
+    title = "Test Mail"
+    subject = "Antsoul Error Report"
+
+    # 发送邮件
+    for item in email:
+        commend = r'echo -e "To: \"%s\" <%s>\nFrom: \"%s\" <%s>\nSubject: %s\n\n`cat %s`" | /usr/sbin/sendmail -t' %(title, item, from_name, fr, subject, tmpfile)
+        print(commend)
+        system(commend)
     # remove(tmpfile)
 
-sendErrorInfo("123123")
+if __name__ == "__main__":
+    sendErrorInfo("123123")
--- testantsoul.py
+++ testantsoul.py
@@ -1,30 +1,49 @@
-###¸Ã½Å±¾ÓÃÓÚ²âÊÔÂìÒÏptÊÇ·ñÕı³£¹¤×÷###
-
-import urllib.request as request
-from os import system  
+# coding=utf-8
+###è¯¥è„šæœ¬ç”¨äºæµ‹è¯•èš‚èšptæ˜¯å¦æ­£å¸¸å·¥ä½œ###
+import urllib2
+import urllib
+from os import system
+import SendMail
+import cookielib
 import re
 
-url="http://pt.antsoul.com/index.php"
+def testError(content):
+    """
+    æµ‹è¯•é”™è¯¯ä¿¡æ¯
+    :param content: bool, True -> Error; False -> No Error
+    """
+    rp = re.compile("Memcache"), re.compile("MySql Error")
+    for pt in rp:
+        rs = pt.findall(content)
+        if rs:
+            return True
+        else:
+            return False
+
+def dealError(content):
+    print('Server Error')
+    SendMail.sendErrorInfo(res)                             # å‘é€é”™è¯¯æ•°æ®
+
+# def Init():
+#     cj = cookielib.CookieJar()
+#     opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+#     urllib2.install_opener(opener)
+url="https://pt.antsoul.com/login.php"
 try:
-    headers = ('User-Agent','Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11')
-    opener = request.build_opener()
-    opener.addheaders = [headers]
-    response = opener.open(url).read().decode("utf8")
-    result = re.findall(r'Magi Madoka Magica',response)
-   # print(response)
+    headers = { 'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11' }
+    req = urllib2.Request(url, headers = headers)
+    res = urllib2.urlopen(req).read().decode("UTF-8")           # è¿”å›çš„æ•°æ®å†…å®¹
+    # print(res.encode("UTF-8"))
    
-    if result:
-       # print(result)
-        print("It's Ok")
+    if not testError(res):                                          # å¤„ç†é”™è¯¯
+        dealError(res)
     else:
-        print('Server Error')
-       # system('reboot')
-except request.URLError as e:
+        print("OK!")
+
+except urllib2.URLError as e:
     print ("URL ERROR\n")
     print (e.reason)
-   # system('reboot')
-except request.HTTPError as e:
+except urllib2.HTTPError as e:
     print ("HTTP ERROR")
     print (e.reason)
-   # system('reboot')
 
